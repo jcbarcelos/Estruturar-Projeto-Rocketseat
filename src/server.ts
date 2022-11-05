4f8087e11e1fcb6280c64fc2cb98c5c598c0f7c3
jcbarcelosrj <jcbarcelosrj@gmail.com>
2022-11-05 16:29:43 -0300

    refactor: remove env

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.get("/", (request, response) => {
+  return response.json({
+    message: "hello world",
+  });
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
